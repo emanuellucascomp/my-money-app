10e2a52a9a6d73f28e04a88cd3357a0f1456be78
Emanuel Lucas de Sousa e Silva <cardgameheritage@gmail.com>
2019-03-14 22:21:31 -0300

    billing list

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -9,6 +9,7 @@ import TabsHeader from '../common/tab/tabsHeader';
 import TabsContent from '../common/tab/tabsContent';
 import TabHeader from '../common/tab/tabHeader';
 import TabContent from '../common/tab/tabContent';
+import List from './billingCycleList';
 
 class BillingCycle extends Component {
     componentWillMount(){
@@ -29,7 +30,9 @@ class BillingCycle extends Component {
                             <TabHeader label='Excluir' icon='trash-o' target='tabDelete' />
                         </TabsHeader>
                         <TabsContent>
-                            <TabContent id='tabList'><h1>Lista</h1></TabContent>
+                            <TabContent id='tabList'>
+                                <List />
+                            </TabContent>
                             <TabContent id='tabCreate'><h1>Criar</h1></TabContent>
                             <TabContent id='tabUpdate'><h1>Alterar</h1></TabContent>
                             <TabContent id='tabDelete'><h1>Deletar</h1></TabContent>
