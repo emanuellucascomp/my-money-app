084694b0de1e56b3abce4d134fd0e25ad63fbd26
Emanuel Lucas de Sousa e Silva <cardgameheritage@gmail.com>
2019-03-17 22:12:19 -0300

    list and toastr

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -10,6 +10,8 @@ import TabsContent from '../common/tab/tabsContent';
 import TabHeader from '../common/tab/tabHeader';
 import TabContent from '../common/tab/tabContent';
 import List from './billingCycleList';
+import Form from './billingCycleForm';
+import { create } from './billingCycleActions';
 
 class BillingCycle extends Component {
     componentWillMount(){
@@ -33,7 +35,9 @@ class BillingCycle extends Component {
                             <TabContent id='tabList'>
                                 <List />
                             </TabContent>
-                            <TabContent id='tabCreate'><h1>Criar</h1></TabContent>
+                            <TabContent id='tabCreate'>
+                                <Form onSubmit={this.props.create} />
+                            </TabContent>
                             <TabContent id='tabUpdate'><h1>Alterar</h1></TabContent>
                             <TabContent id='tabDelete'><h1>Deletar</h1></TabContent>
                         </TabsContent>
@@ -44,6 +48,6 @@ class BillingCycle extends Component {
     }
 }
 
-const mapDispatchToProps = dispatch => bindActionCreators({ selectTab, showTabs }, dispatch);
+const mapDispatchToProps = dispatch => bindActionCreators({ selectTab, showTabs, create }, dispatch);
 
 export default connect(null, mapDispatchToProps)(BillingCycle);
--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { toastr } from 'react-redux-toastr';
 const BASE_URL = 'http://localhost:3003/api';
 
 export function getList() {
@@ -6,5 +7,20 @@ export function getList() {
     return {
         type: 'BILLING_CYCLES_FETCHED',
         payload: request
+    }
+}
+
+export function create(values){
+    axios.post(`${BASE_URL}/billingCycles`, values)
+        .then(res => {
+            toastr.success('Sucesso', 'Operação realizada com sucesso.');
+        })
+        .catch(err => {
+            err.response.data.errors.forEach(element => {
+                toastr.error('Erro', element);           
+            })
+        });
+    return {
+        type: 'TEMP'
     }
 }
