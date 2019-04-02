115e2d375754e084858fbda0abbdd2c6c563c62f
Emanuel Lucas de Sousa e Silva <cardgameheritage@gmail.com>
2019-04-02 11:26:15 -0300

    auth frontend finished

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,18 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { applyMiddleware, createStore } from 'redux';
-import { Provider } from 'react-redux';
-import reducers from './main/reducers';
-import App from './main/App';
-import promise from 'redux-promise';
-import multi from 'redux-multi';
-import thunk from 'redux-thunk';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { applyMiddleware, createStore } from 'redux'
+import { Provider } from 'react-redux'
 
-const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-const store = applyMiddleware(multi, thunk, promise)(createStore)(reducers, devTools);
+import promise from 'redux-promise'
+import multi from 'redux-multi'
+import thunk from 'redux-thunk'
 
+import Routes from './main/routes'
+import reducers from './main/reducers'
+
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ 
+      && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = applyMiddleware(multi, thunk, promise)(createStore)(reducers, devTools)
 ReactDOM.render(
     <Provider store={store}>
-        <App />
-    </Provider>   
-, document.getElementById('app'));
+        <Routes />
+    </Provider>
+, document.getElementById('app'))
--- frontend/src/main/routes.jsx
+++ frontend/src/main/routes.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { Router, Route, Redirect, hashHistory } from 'react-router';
 import Dashboard from '../dashboard/dashboard';
 import BillingCycle from '../billingCycle/billingCycle';
+import AuthOrApp from './authOrApp';
 
 export default props => (
     <Router history={hashHistory}>
-        <Route path='/' component={Dashboard} />
+        <Route path='/' component={AuthOrApp} />
         <Route path='/billingCycles' component={BillingCycle} />
         <Redirect from='*' to='/' />
     </Router>
